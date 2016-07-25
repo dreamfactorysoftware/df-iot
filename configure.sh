e9ad90cd242045ca02f9908e23c0c79b7088caab
Janakiram MSV <janakiramm@users.noreply.github.com>
2016-07-25 15:30:26 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -12,8 +12,10 @@ make test
 sudo make install
 cd utils
 sudo ./install_server.sh
-sudo service redis_6379 start
-sudo service redis_6379 stop
+sudo service redis_6379 restart
+cd 
+sudo rm redis-stable.tar.gz
+sudo rm -rf redis-stable
 
 
 # install latest version of Node.js
@@ -34,10 +36,10 @@ sudo /opt/bitnami/mongodb/scripts/ctl.sh start
 sudo mongo < /opt/bitnami/apps/dreamfactory/df-iot/df-iot-mongo.js
 
 # configure Mosca
+cd /opt/bitnami/apps/dreamfactory/df-iot
 token=$(curl  -X POST "http://localhost/api/v2/system/admin/session" -d '{ "email" : "user@example.com", "password" : "bitnami", "remember_me": true }' -H "Content-Type: application/json" | jq '{session_token}' | jq '.session_token' | tr -d '"')
-cp config.json.template config.json
-sed -i "s/DreamFactory_TOKEN/$token/" config.json
+sudo cp config.json.template config.json
+sudo sed -i "s/DreamFactory_URL/localhost/" config.json
+sudo sed -i "s/DreamFactory_TOKEN/$token/" config.json
 token=
 node index.js --config config.json
-
-
