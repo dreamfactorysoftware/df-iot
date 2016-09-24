1c0137a18b1cc3b43c9b9eab2d69929018bbfc0f
Janakiram MSV <janakiramm@users.noreply.github.com>
2016-09-24 08:53:47 +0530

    Added reset script

---
+++ config/reset-env.sh
@@ -0,0 +1,19 @@
+token=$(curl  -X POST "http://localhost/api/v2/system/admin/session" -d '{ "email" : "user@example.com", "password" : "bitnami", "remember_me": true }' -H "Content-Type: application/json" | jq '{session_token}' | jq '.session_token' | tr -d '"')
+
+# Delete MongoDB Collections
+sudo mongo < /opt/bitnami/apps/dreamfactory/df-iot/config/df-iot-mongo-reset.js
+
+# Delete Freeboard
+sudo rm -rf /opt/bitnami/apps/freeboard/
+sudo cp /opt/bitnami/apache2/conf/bitnami/bitnami-apps-prefix.bak /opt/bitnami/apache2/conf/bitnami/bitnami-apps-prefix.conf
+sudo sh /opt/bitnami/ctlscript.sh restart apache
+sudo rm -rf /opt/bitnami/apps/dreamfactory/htdocs/storage/app/Freeboard
+
+# Delete DreamFactory IoT Services
+devices_svc_id=$(curl -X GET --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-DreamFactory-Api-Key: 36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88' --header "X-DreamFactory-Session-Token: $token" --header 'Authorization: Basic dXNlckBleGFtcGxlLmNvbTpiaXRuYW1p' 'http://localhost/api/v2/system/service' | jq '.resource[] | select(.name=="devices")'| jq .id)
+messages_svc_id=$(curl -X GET --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-DreamFactory-Api-Key: 36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88' --header "X-DreamFactory-Session-Token: $token" --header 'Authorization: Basic dXNlckBleGFtcGxlLmNvbTpiaXRuYW1p' 'http://localhost/api/v2/system/service' | jq '.resource[] | select(.name=="messages")'| jq .id)
+telemetry_svc_id=$(curl -X GET --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-DreamFactory-Api-Key: 36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88' --header "X-DreamFactory-Session-Token: $token" --header 'Authorization: Basic dXNlckBleGFtcGxlLmNvbTpiaXRuYW1p' 'http://localhost/api/v2/system/service' | jq '.resource[] | select(.name=="telemetry")'| jq .id)
+curl -X DELETE --header 'Accept: application/json' --header 'X-DreamFactory-Api-Key: 36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88' --header "X-DreamFactory-Session-Token: $token" "http://localhost/api/v2/system/service?force=false&ids=$devices_svc_id"
+curl -X DELETE --header 'Accept: application/json' --header 'X-DreamFactory-Api-Key: 36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88' --header "X-DreamFactory-Session-Token: $token" "http://localhost/api/v2/system/service?force=false&ids=$messages_svc_id"
+curl -X DELETE --header 'Accept: application/json' --header 'X-DreamFactory-Api-Key: 36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88' --header "X-DreamFactory-Session-Token: $token" "http://localhost/api/v2/system/service?force=false&ids=$telemetry_svc_id"
+
